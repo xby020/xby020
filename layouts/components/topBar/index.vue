824b65a5013fc4ed3b175c583016b04eda21e53a
xby020 <xby020@gmail.com>
2021-07-18 23:26:17 +0800

    确定主页结构

---
+++ layouts/components/topBar/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar app flat dark color="#1B2021">
+    <!-- logo -->
+    banana
+    <v-spacer></v-spacer>
+    <v-switch label="darkMode" v-model="isDark"></v-switch>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "topBar",
+  data() {
+    return {
+      isDark: false
+    };
+  },
+  watch: {
+    isDark: {
+      handler(val) {
+        this.$vuetify.theme.dark = val;
+      },
+      immediate: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,64 +1,65 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - xby020',
-    title: 'xby020',
+    titleTemplate: "%s - xby020",
+    title: "xby020",
     htmlAttrs: {
-      lang: 'en'
+      lang: "zh-CN"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    // https://www.tailwindcss.cn/docs/guides/nuxtjs
+    "@nuxtjs/tailwindcss",
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
       dark: true,
       themes: {
+        light: {
+          primary: "#EEEEEE",
+          accent: "#50CB93",
+          secondary: "#57837B",
+          info: "#5F939A",
+          warning: "#FFC107",
+          error: "#FB3640",
+          success: "#81B214"
+        },
         dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+          primary: "#2C2E43",
+          accent: "#1E6F5C",
+          secondary: "#56776C"
         }
       }
     }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {}
+};
